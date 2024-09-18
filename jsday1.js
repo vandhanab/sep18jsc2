4cff219c8b5ea3d3acd5f598c35c349372672227
vandhanab <112164783+vandhanab@users.noreply.github.com>
2024-09-18 22:15:46 +0530

    Add files via upload

---
+++ jsday1.js
@@ -0,0 +1,53 @@
+var a=["Nanna","Amma","sister","me",{sister:1,me:2}]
+
+var max={
+    parents:{
+     Nanna:{
+        name:"Janakirami Reddy",
+        age:"49",
+        occupation:"rice business",
+     },
+     Amma:{
+        name:"Nagamani",
+        age:"40",
+        occupation:"house wife",
+     }
+    },
+    child:{
+        sister:{
+            name:"priyanka",
+            age:"22",
+        },
+        me:{
+            name:"vandana",
+            age:"21",
+        }
+    }
+}
+ console.log(a);
+ console.log(a[4]["me"]);
+ console.log(max.parents.Nanna);
+ console.log(max.parents.Amma);
+ console.log(max.child.sister);
+ console.log(max.child.me);
+ console.log(a[0]);
+ console.log(a[1]);
+ console.log(a[2]);
+ console.log(a[3]);
+ console.log(a[4]["sister"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
